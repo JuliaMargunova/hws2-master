1c18cb8a33e0f20b76185f4192b8bf457d3637e9
Julia <yulia.bobko@mail.ru>
2023-05-30 16:55:05 +0300

    HW10
    style loader
    loadingReducer
    useSelector
    useDispatch

--- src/s2-homeworks/hw10/Loader.tsx
+++ src/s2-homeworks/hw10/Loader.tsx
@@ -1,3 +1,10 @@
 import s from './Loader.module.css'
+import preloading from './/images/ellipse.svg'
 
-export const Loader = () => <div className={s.loader}/>
+//export const Loader = () => <div className={s.loader}></div>
+//export const Loader = () => <div className={s.preloadingBlock_el}><img src={preloading}/> </div>
+export const Loader = () => {
+    return (
+        <span className={s.loader}></span>
+    );
+};
